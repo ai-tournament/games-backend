76f05a272675163eb6becfc8401484181be6ae5c
João Paulo Leonidas Fernandes Dias da Silva <jsilva@thoughtworks.com>
2016-03-31 11:17:40 -0300

    Adds example of url routing and in-memory database.

--- project.clj
+++ project.clj
@@ -4,7 +4,12 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [compojure "1.4.0"]
-                 [ring/ring-defaults "0.1.5"]]
+                 [ring/ring-defaults "0.1.5"]
+                 [com.mchange/c3p0 "0.9.2.1"]
+                 [cheshire "5.5.0"]
+                 [ring/ring-json "0.4.0"]
+                 [org.clojure/java.jdbc "0.2.3"]
+                 [com.h2database/h2 "1.3.170"]]
   :plugins [[lein-ring "0.9.7"]]
   :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"]
   :ring {:handler games-api.handler/app}
